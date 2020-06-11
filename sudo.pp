9c8c4fdd336239110a82295702551b889fefe087
Mohammad Khabir Uddin <masumndc1@gmail.com>
2020-06-11 10:56:46 +0300

    installing sudo and configure

---
+++ sudo.pp
@@ -0,0 +1,11 @@
+#installing and configure sudo and add user in it
+
+$user = "masum"
+
+package { 'sudo':
+  ensure => 'present',
+}
+
+file { "/usr/local/etc/sudoers.d/10_$user":
+  content => "$user ALL=(ALL:ALL) NOPASSWD: ALL",
+}
