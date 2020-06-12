80a0d027463fcdebab450e7edf4b26eec5f47444
masumndc1 <masumndc1@gmail.com>
2020-06-12 17:09:26 +0300

    dependency added to service.pp

--- service.pp
+++ service.pp
@@ -1,7 +1,13 @@
 #keeping service up
 
+package { 'puppet':
+  ensure => 'latest',
+  before => Service['puppet'],
+}
+
 service { 'puppet':
-  enable => 'true',
-  ensure => 'running',
-  tag    => 'puppet',
+  enable  => 'true',
+  ensure  => 'running',
+  require => Package['puppet'],
+  tag     => 'puppet',
 }
