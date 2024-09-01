a6f525580fbb9f2c66e08c175944a59980f0614d
Khabir Uddin <khuddin@csc.fi>
2024-09-01 22:20:16 +0300

    summary manifest file changes

--- manifests/summary.pp
+++ manifests/summary.pp
@@ -1,11 +1,11 @@
-class (
-  $_arch = $facts['os']['architechture']
-  $_uptime = $facts['system_uptime']
-  $_family = $facts['os']['family']
-  $_name = $ facts['os']['name']
-  $_release = $ facts['os']['release']['full']
-  $_distro = $ facts['os']['distro']['codename']
-) infra::summary {
+class infra::summary (
+  $_arch = $facts['os']['architechture'],
+  $_uptime = $facts['system_uptime']['uptime'],
+  $_family = $facts['os']['family'],
+  $_name = $facts['os']['name'],
+  $_release = $facts['os']['release']['full'],
+  $_distro = $facts['os']['distro']['codename']
+) {
   file { '/home/root/summary.txt':
     content => template('infra/summary.erb')
   }
