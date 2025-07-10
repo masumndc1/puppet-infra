252a7ff32b7b48198e926326f0119a5df742b72b
Khabir Uddin <khuddin@csc.fi>
2025-07-11 01:13:40 +0300

    dont use lazyvim for a while

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,10 @@
 class infra {
   include infra::hostname
-  include infra::neovim
   include infra::packages
   include infra::partition
   include infra::sudo
   include infra::summary
   include infra::zsh
   include infra::messages
+  # include infra::neovim
 }
