b6e07af2ff6aebd6659e2aa0b5608db1a2d885d6
Khabir Uddin <khuddin@csc.fi>
2025-09-08 00:30:30 +0300

    deactivate these two manifest just for now

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,10 @@
 class infra {
-  include infra::hostname
+  #include infra::hostname
   include infra::packages
   include infra::partition
   include infra::sudo
   include infra::summary
   include infra::zsh
-  include infra::messages
+  #include infra::messages
   # include infra::neovim
 }
